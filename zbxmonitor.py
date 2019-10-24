d753155eb8f164d74d96f872d22eaeef7c4c0b25
marekpl <marekpl@nask.pl>
2019-10-24 19:30:46 +0200

    2019-10-24 19:30:46

--- zbxmonitor.py
+++ zbxmonitor.py
@@ -1,48 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-"""
-Zabbix monitor client which talks with zabbix server using API provided by zabbix.
-Creates icon in tray which represents current status of monitored server.
-
-OPTIONS
-    -h|--host
-        hostname or ip
-    -c|--command
-        start/stop. Default is start.
-    -u|--user
-        zabbix user name. Should has read-only permission to all monitored hosts
-        and should has not access to zabbix WEB GUI.
-    -P|--password
-        password for zabbix user. If omitted you'll be asked for password.
-        It's strongly recommended do omit this option, because your
-        password will be visible in process list.
-    -C|--credentials
-        path to credentials file. Only first line is read (password). File name
-        may be absolute path or file in client "home" directory
-    Options: host and user are mandatory. Option password has no default value.
-    -i|--interval
-        pooling interval [s]. Default is 60.
-    -p|--port
-        communication port. Default is 10051
-    -I|--icon
-        icon name for OK and ERR status. Icons must be found in client "home"
-        directory and must be *.png files. File names are calculated as:
-        "homedir/icon-ok.png" and "homedir/icon-err.png", so with
-        option --icon=myicon you should have two files: myicon-ok.png and
-        myicon-err.png i client home directory. Default icon name is "icon".
-    -n|--notify
-        If set sends "balloon" notifies. If not, shows icon in tray only.
-        Default false.
-    -t|--text
-        text mode when pick user password. Useful for zab-expect
-    -W|--wav
-        *.wav files for OK and ERR status. File names are calculated like icon
-        files. Default none. If not set no sound will be played.
-    -w|--wav-player
-        player for *.wav files. Must be installed. Default is "/usr/bin/mpv".
-
-"""
+#!/usr/bin/python -W ignore
 
 from socket import *
 from pyzabbix import *
@@ -57,10 +15,11 @@ import gobject
 import notify2
 import syslog
 import ConfigParser
+import warnings
 from dialog_nix import *
 
 
-class DataContainer:
+class Globals:
     def __init__(self, script_name):
         self.script_full_path = os.path.abspath(script_name)
         self.script_dir = os.path.dirname(self.script_full_path)
@@ -148,17 +107,43 @@ class DataContainer:
                 self.zbxpasswd = mypass[0]
 
 
+class TrayTxt:
+    def __init__(self, command):
+        zbx.status()
+        self.flog = open(globals.zbxlog, "a")
+        self.flog.write("\n" + globals.script_name + ": ")
+        self.flog.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())))
+        self.flog.write(" on " + globals.zbxhost + " " + command + "\n")
+        self.flog.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())) + " ")
+        self.flog.write(globals.zbx_status)
+        self.flog.flush()
+        # globals.script_name + ":", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())), "on", globals.zbxhost, command
+
+    def check(self):
+        zbx.status()
+        syslog.syslog(globals.zbxhost + " status (txt): " + globals.zbx_status)
+        if globals.zbx_status != globals.zbx_last_status:
+            globals.zbx_last_status = globals.zbx_status
+            self.flog.write(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())) + " ")
+            self.flog.write(globals.zbx_status)
+            self.flog.flush()
+        return globals.zbx_status
+
+    def tray(self):
+        gobject.timeout_add(globals.zbxinterval, self.check)
+        gobject.MainLoop().run()
+
+
 class GtkMessages:
     def __init__(self):
         self.statusIcon = gtk.StatusIcon()
         self.statusIcon.connect('activate', self.show_current_stat)
         self.statusIcon.connect('popup-menu', self.on_right_click)
         zbx.status()
-        if dc.zbx_status != "ok":
-            # self.set_icon(dc.icon_err)
-            self.set_icon(dc.zbxicon + "-err.png")
+        if globals.zbx_status != "ok":
+            self.set_icon(globals.zbxicon + "-err.png")
         else:
-            self.set_icon(dc.zbxicon + "-ok.png")
+            self.set_icon(globals.zbxicon + "-ok.png")
 
     def set_icon(self, icon_file):
         self.statusIcon.set_from_file(icon_file)
@@ -171,10 +156,10 @@ class GtkMessages:
 
     def make_menu(self, event_button, event_time, data=None):
         menu = gtk.Menu()
-        reconnect_item = gtk.MenuItem("Reconnect to " + dc.zbxhost)
-        close_item = gtk.MenuItem("Close applet " + dc.zbxhost)
+        reconnect_item = gtk.MenuItem("Reconnect to " + globals.zbxhost)
+        close_item = gtk.MenuItem("Close applet " + globals.zbxhost)
 
-        if dc.zbx_connected != 'ok' and dc.zbx_ping == 'ok':
+        if globals.zbx_connected != 'ok' and globals.zbx_ping == 'ok':
             menu.append(reconnect_item)
             # add callback
             reconnect_item.connect_object("activate", self.reconnect_to_zbxhost, "reconnect to zbx server")
@@ -190,42 +175,24 @@ class GtkMessages:
 
     def show_current_stat(self, event):
         zbx.status()
-        if dc.zbx_status != "ok":
-            self.set_icon(dc.zbxicon + "-err.png")
+        if globals.zbx_status != "ok":
+            self.set_icon(globals.zbxicon + "-err.png")
         else:
-            self.set_icon(dc.zbxicon + "-ok.png")
-        self.message(dc.zbxhost + " current status is:\n\n" + dc.zbx_status)
+            self.set_icon(globals.zbxicon + "-ok.png")
+        self.message(globals.zbxhost + " current status is:\n\n" + globals.zbx_status)
 
     def on_right_click(self, data, event_button, event_time):
         self.make_menu(event_button, event_time)
 
     def reconnect_to_zbxhost(self, data=None):
         zbx.login()
-        self.message('Logging to ' + dc.zbxhost + ":\n" + dc.zbx_connected)
+        self.message('Logging to ' + globals.zbxhost + ":\n" + globals.zbx_connected)
 
     def close_app(self, data=None):
-        syslog.syslog(dc.zbxhost + ": disconnected.")
+        syslog.syslog(globals.zbxhost + ": disconnected.")
         gtk.main_quit()
         # if self.message(data, gtk.BUTTONS_OK_CANCEL) == gtk.RESPONSE_OK:
         #     gtk.main_quit()
-
-
-class TrayTxt:
-    def __init__(self):
-        # self.fh = fh
-        self.flog = open(dc.zbxlog, "w")
-
-    def check(self):
-        zbx.status()
-        syslog.syslog(dc.zbxhost + " status (txt): " + dc.zbx_status)
-        if dc.zbx_status != dc.zbx_last_status:
-            dc.zbx_last_status = dc.zbx_status
-            self.flog.write(dc.zbx_status)
-            self.flog.flush()
-        return dc.zbx_status
-
-    def tray(self):
-        gobject.timeout_add(dc.zbxinterval, self.check)
 
 
 class TrayIcon:
@@ -235,77 +202,78 @@ class TrayIcon:
 
     def check(self):
         zbx.status()
-        syslog.syslog(dc.zbxhost + " status (GUI): " + dc.zbx_status)
-        if dc.zbx_status != dc.zbx_last_status:
-            dc.zbx_last_status = dc.zbx_status
-            if dc.zbxnotify:
-                n = notify2.Notification("Zabbix: " + dc.zbxhost, dc.zbx_status)
-                if dc.zbx_status == "ok":
+        syslog.syslog(globals.zbxhost + " status (GUI): " + globals.zbx_status)
+        if globals.zbx_status != globals.zbx_last_status:
+            globals.zbx_last_status = globals.zbx_status
+            if globals.zbxnotify:
+                n = notify2.Notification("Zabbix: " + globals.zbxhost, globals.zbx_status)
+                if globals.zbx_status == "ok":
                     n.set_urgency(1)
                 else:
                     n.set_urgency(2)
                     n.timeout = -1
                 n.show()
-            # if dc.play_wav:
-            if dc.zbxwav is not None:
+            # if globals.play_wav:
+            if globals.zbxwav is not None:
                 try:
                     f = open('/dev/null', 'w')
-                    if dc.zbx_status == 'ok':
-                        call([dc.zbxwav_player, dc.zbxwav + "-ok.wav"], stdout=f, stderr=f)
+                    if globals.zbx_status == 'ok':
+                        call([globals.zbxwav_player, globals.zbxwav + "-ok.wav"], stdout=f, stderr=f)
                     else:
-                        call([dc.zbxwav_player, dc.zbxwav + "-err.wav"], stdout=f, stderr=f)
+                        call([globals.zbxwav_player, globals.zbxwav + "-err.wav"], stdout=f, stderr=f)
                 except:
                     pass
-        if dc.zbx_status == 'ok':
-            self.__gmsg.set_icon(dc.zbxicon + "-ok.png")
+        if globals.zbx_status == 'ok':
+            self.__gmsg.set_icon(globals.zbxicon + "-ok.png")
         else:
-            self.__gmsg.set_icon(dc.zbxicon + "-err.png")
+            self.__gmsg.set_icon(globals.zbxicon + "-err.png")
         return True
 
     def tray(self):
-        gobject.timeout_add(dc.zbxinterval, self.check)
+        gobject.timeout_add(globals.zbxinterval, self.check)
         gtk.main()
 
 
 class MyZbx:
     def __init__(self):
-        self.zapi = ZabbixAPI(dc.zbxurl, timeout=5)
+        self.zapi = ZabbixAPI(globals.zbxurl, timeout=5)
         self.zapi.session.verify = False
+        warnings.filterwarnings("ignore")
         self.login()
 
     def pingit(self):
         sock = socket(AF_INET, SOCK_STREAM)
         sock.settimeout(2)
         try:
-            sock.connect((dc.zbxhost, dc.zbxport))
-            dc.zbx_ping = 'ok'
+            sock.connect((globals.zbxhost, globals.zbxport))
+            globals.zbx_ping = 'ok'
         except:
-            dc.zbx_ping = 'port is down'
+            globals.zbx_ping = 'port is down'
         finally:
             sock.close()
 
     def login(self):
         self.pingit()
-        if dc.zbx_ping == 'ok':
+        if globals.zbx_ping == 'ok':
             try:
-                self.zapi.login(dc.zbxuser, dc.zbxpasswd)
-                dc.zbx_connected = 'ok'
-                dc.zbx_ver = self.zapi.api_version().split('.')[0]
+                self.zapi.login(globals.zbxuser, globals.zbxpasswd)
+                globals.zbx_connected = 'ok'
+                globals.zbx_ver = self.zapi.api_version().split('.')[0]
             except:
-                dc.zbx_connected = 'not logged in'
+                globals.zbx_connected = 'not logged in'
             finally:
-                print "Connect: " + dc.zbx_connected
+                print "Connect: " + globals.zbx_connected
 
     def status(self):
         self.pingit()
-        if dc.zbx_ping != 'ok':
-            dc.zbx_status = dc.zbx_ping
-            return dc.zbx_status
-        if dc.zbx_connected == 'not logged in':
-            dc.zbx_status = dc.zbx_connected
-            return dc.zbx_status
-        dc.zbx_status = self.get_triggers(dc.zbx_ver)
-        return dc.zbx_status
+        if globals.zbx_ping != 'ok':
+            globals.zbx_status = globals.zbx_ping
+            return globals.zbx_status
+        if globals.zbx_connected == 'not logged in':
+            globals.zbx_status = globals.zbx_connected
+            return globals.zbx_status
+        globals.zbx_status = self.get_triggers(globals.zbx_ver)
+        return globals.zbx_status
 
     def get_triggers(self, zbx_ver):
         try:
@@ -352,10 +320,10 @@ class MyZbx:
                                                    )
             else:
                 return "Unknown ZBX ver. (" + zbx_ver + ")"
-            dc.zbx_connected = 'ok'
+            globals.zbx_connected = 'ok'
         except:
-            dc.zbx_connected = "Fetch data error."
-            return dc.zbx_connected
+            globals.zbx_connected = "Fetch data error."
+            return globals.zbx_connected
 
         unack_trigger_ids = [t['triggerid'] for t in unack_triggers]
         for t in triggers:
@@ -389,17 +357,13 @@ class MyZbx:
 
 class myDaemon(Daemon):
     def run(self):
-        syslog.openlog(dc.script_name, syslog.LOG_PID | syslog.LOG_NDELAY, syslog.LOG_DAEMON)
+        syslog.openlog(globals.script_name, syslog.LOG_PID | syslog.LOG_NDELAY, syslog.LOG_DAEMON)
         tc.tray()
 
 
 def main(argv):
-    global dc, zbx, tc
+    global globals, zbx, tc
     # command - argv[1] (start if no args)
-
-    def usage():
-        print(globals()['__doc__'])
-        sys.exit(2)
 
     if len(argv) == 1:
         command = "start"
@@ -412,25 +376,24 @@ def main(argv):
         print "Unknown command"
         sys.exit(2)
 
-    dc = DataContainer(argv[0])
-    print dc.script_name + ":", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())), "on", dc.zbxhost, command
+    globals = Globals(argv[0])
+    print globals.script_name + ":", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())), "on", globals.zbxhost, command
 
     zbx = MyZbx()
-    if (dc.zbxtext_mode):
-        # flog = open(dc.zbxlog, "w")
-        # tc = TrayTxt(flog)
-        tc = TrayTxt()
+    if (globals.zbxtext_mode):
+        tc = TrayTxt(command)
     else:
         tc = TrayIcon()
-    # quit()
 
-    pidfile = "/tmp/" + dc.script_name + "." + dc.zbxhost + ".pid"
-    # daemon = myDaemon("/tmp/" + dc.script_name + "." + dc.zbxhost + ".pid")
-    daemon = myDaemon(pidfile, stderr="/tmp/stdout", stdout="/tmp/stdout")
+    f = globals.tmp_dir + "/" + globals.script_name + "." + globals.zbxhost
+    pidfile = f + ".pid"
+    stdoutfile = f  + ".out"
+    stderrfile = f + ".log"
+    daemon = myDaemon(pidfile, stderr=stderrfile, stdout=stdoutfile)
     if 'start' == command:
-        daemon.start(dc.script_name, dc.zbxhost)
+        daemon.start(globals.script_name, globals.zbxhost)
     elif 'stop' == command:
-        daemon.stop(dc.zbxhost)
+        daemon.stop(globals.zbxhost)
     else:
         print "Unknown command"
         sys.exit(2)
